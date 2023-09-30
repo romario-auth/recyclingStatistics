78b4878bf9df49750c43b96e5f52bc1a73fc3936
Anderson Damasceno <83956940+Andersonmelodamasceno@users.noreply.github.com>
2023-09-30 16:28:27 -0300

    Criei uma  nova controladora chamada EcoCycle

---
+++ src/Recycle.Api/Recycle.Api/Controllers/EcoCycleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Recycle.Api.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class EcoCycleController : ControllerBase
+    {
+        public EcoCycleController()
+        {
+        }
+
+        [HttpGet(Name = "FirstPoint")]
+        public string EcoPoint()
+        {
+            return "Segundo ponto";
+            
+        }
+    }
+
+}
